5ae9edac38357d543b4ff0e18f2d63a133ed38ad
Sven Fiergolla <sven.fiergolla@gmail.com>
2022-06-19 13:45:30 +0200

    Added fastAPI for remote trigger and event loop

--- roomba/main.py
+++ roomba/main.py
@@ -1,43 +1,102 @@
 import asyncio
-import json
 import logging
-import time
+import pigpio
+from starlette import status
+
 from roomba import Roomba
+from fastapi import FastAPI
 
-if __name__ == '__main__':
+app = FastAPI(title="test", description="test", version="0.0.1")
+STATE = 0
+GREEN = 22
+RED = 17
 
-    # Uncomment the following two lines to see logging output
+
+@app.get("/run", status_code=status.HTTP_201_CREATED)
+async def get():
+    global STATE
+    STATE = 1
+
+    return
+
+
+async def run():
+    await init()
+    global STATE
+
+    while STATE != -1:
+
+        logging.info(f'current event loop state: {STATE}')
+
+        if STATE == 1:
+            await start()
+        elif STATE == 2:
+            await stop()
+        elif STATE == 3:
+            await dock()
+        else:
+            for i in range(2):
+                # print(json.dumps(roomba.master_state, indent=2))
+                await asyncio.sleep(1)
+
+    roomba.disconnect()
+    pi.stop()
+
+
+async def dock():
+    await asyncio.sleep(30)
+    roomba.send_command("dock")
+
+
+async def stop():
+    pi.set_PWM_dutycycle(GREEN, 0)
+    pi.set_PWM_dutycycle(RED, 255)
+
+    await asyncio.sleep(30)
+    roomba.send_command("stop")
+
+
+async def start():
+    roomba.send_command("start")
+    pi.set_PWM_dutycycle(GREEN, 255)
+
+
+async def init():
+    roomba.connect()
+    await asyncio.sleep(10)
+    roomba.set_preference("carpetBoost", "true")
+    roomba.set_preference("twoPass", "true")
+
+
+def main():
+    global roomba
+    global pi
+
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
     # uncomment the option you want to run, and replace address, blid and roombaPassword with your own values
-
     address = "192.168.0.132"
     blid = "EEB15740BF3548A982C6BF99A916979C"
     roombaPassword = ":1:1653325360:nzUEQFIwZWTLn0t8"
 
-    myroomba = Roomba(address, blid, roombaPassword)
-
-
     # myroomba = Roomba(address) #if you have a config file - will attempt discovery if you don't
-
-    async def test():
-        myroomba.connect()
-        await asyncio.sleep(10)
-        myroomba.set_preference("carpetBoost", "true")
-        myroomba.set_preference("twoPass", "false")
-
-        myroomba.send_command("start")
-        await asyncio.sleep(30)
-        myroomba.send_command("stop")
-        await asyncio.sleep(30)
-        myroomba.send_command("dock")
-
-        import json, time
-        for i in range(10):
-            print(json.dumps(myroomba.master_state, indent=2))
-            await asyncio.sleep(1)
-        myroomba.disconnect()
-
+    roomba = Roomba(address, blid, roombaPassword)
+    pi = pigpio.pi()
 
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(test())
+    loop.create_task(run())
+
+    try:
+        loop.run_forever()
+
+    except (KeyboardInterrupt, SystemExit):
+        logging.info("System exit Received - Exiting program")
+        roomba.disconnect()
+        logging.info('Program Exited')
+
+    finally:
+        pass
+
+
+if __name__ == '__main__':
+    main()
